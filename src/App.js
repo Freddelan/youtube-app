690d53337bbc261e8be3491454ed363cec97b2c4
Freddelan <happyrush@hotmail.com>
2023-06-05 14:22:45 +0200

    Beautiful but not perfect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Navbar from "./Components/Layouts/Navbar";
 import logoyt from "./Assets/Img/logoYt.png";
+import scope from "./Assets/Img/scope.png";
+import log from "./Assets/Img/log.png";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Navbar elsrc={logoyt} elalt="logo youtube" />
+        <Navbar
+          elsrc={logoyt}
+          elalt="logo youtube"
+          elsrc1={scope}
+          elalt1="icone scope"
+          elsrc2={log}
+          elalt2="icone log"
+        />
       </div>
-      <div></div>
     </div>
   );
 }
--- src/Components/Layouts/Navbar.js
+++ src/Components/Layouts/Navbar.js
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 
 const Navbar = (props) => {
   const [isInputFocused, setInputFocused] = useState(false);
-
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [showTooltipMicrophone, setShowTooltipMicrophone] = useState(false);
+  const [showTooltipScope, setShowTooltipScope] = useState(false);
+  const [showTooltipNotification, setShowTooltipNotification] = useState(false);
   const handleInputFocus = () => {
     setInputFocused(true);
   };
@@ -11,14 +14,37 @@ const Navbar = (props) => {
     setInputFocused(false);
   };
 
+  const handleMouseEnterMicrophone = () => {
+    setShowTooltipMicrophone(true);
+  };
+
+  const handleMouseLeaveMicrophone = () => {
+    setShowTooltipMicrophone(false);
+  };
+
+  const handleMouseEnterScope = () => {
+    setShowTooltipScope(true);
+  };
+
+  const handleMouseLeaveScope = () => {
+    setShowTooltipScope(false);
+  };
+
+  const handleMouseEnterNotification = () => {
+    setShowTooltipNotification(true);
+  };
+
+  const handleMouseLeaveNotification = () => {
+    setShowTooltipNotification(false);
+  };
+
   return (
     <div className="container flex items-center">
-      <div className="menuBurger relative">
-        <div className="p-2 rounded-full hover:bg-blue-500 transition-colors duration-300">
+      <div className="menuBurger relative ml-5">
+        <div className=" p-3 rounded-full hover:bg-gray-200 transition-colors duration-300">
           <img
-            className="ml-6 mb-2"
-            width="30"
-            height="30"
+            className=" "
+            width="32"
             src="https://img.icons8.com/ios-glyphs/30/menu--v1.png"
             alt="menu--v1"
           />
@@ -32,7 +58,7 @@ const Navbar = (props) => {
         />
       </div>
 
-      <div className="relative text-2xl ml-36 flex items-center">
+      <div className="relative  text-2xl ml-28 flex items-center">
         <input
           type="text"
           className={`px-6 py-2 w-64 md:w-auto custom-width h-14 border border-gray-300 rounded-l-full focus:outline-none transition-all duration-300 ${
@@ -43,7 +69,7 @@ const Navbar = (props) => {
           onFocus={handleInputFocus}
           onBlur={handleInputBlur}
         />
-        <button className="px-6 py-2 h-14 font-thin bg-gray-300 border border-gray-300 rounded-r-full w-24 ">
+        <button className="px-6 py-2 h-14 font-thin bg-gray-300 border border-gray-300 rounded-r-full w-22 ">
           <img
             width="30"
             height="30"
@@ -60,12 +86,81 @@ const Navbar = (props) => {
             alt="search--v1"
           />
         </div>
-        <div className="micro ml-6">
+      </div>
+
+      <div className="relative ml-4 flex items-center">
+        <div
+          className="p-3 rounded-full hover:bg-gray-200 transition-colors "
+          onMouseEnter={handleMouseEnterMicrophone}
+          onMouseLeave={handleMouseLeaveMicrophone}
+        >
           <img
             width="32"
             height="32"
             src="https://img.icons8.com/material-rounded/24/microphone.png"
             alt="microphone"
+          />
+        </div>
+        {showTooltipMicrophone && (
+          <div
+            className="tooltip absolute bg-white border border-gray-300 py-2 px-4"
+            style={{ top: "100%", left: 0 }}
+          >
+            Effectuer une recherche vocale
+          </div>
+        )}
+      </div>
+      <div className="flex">
+        <div className="relative ml-24   ">
+          <div
+            className="p-3 rounded-full hover:bg-gray-200 transition-colors "
+            onMouseEnter={handleMouseEnterScope}
+            onMouseLeave={handleMouseLeaveScope}
+          >
+            <img
+              width="32"
+              height="32"
+              src="https://img.icons8.com/windows/32/camcorder-pro.png"
+              alt="camcorder-pro"
+            />
+          </div>
+          {showTooltipScope && (
+            <div
+              className="tooltip absolute bg-white border border-gray-300 py-2 px-4"
+              style={{ top: "100%", left: 0 }}
+            >
+              Créer
+            </div>
+          )}
+        </div>
+        <div className="absolute ml-44 ">
+          <div
+            className="p-3 rounded-full hover:bg-gray-200 transition-colors "
+            onMouseEnter={handleMouseEnterNotification}
+            onMouseLeave={handleMouseLeaveNotification}
+          >
+            <img
+              width="36"
+              height="38"
+              src="https://img.icons8.com/material-outlined/24/filled-appointment-reminders.png"
+              alt="filled-appointment-reminders"
+            />
+          </div>
+          {showTooltipNotification && (
+            <div
+              className="tooltip  absolute bg-white border border-gray-300 py-2 px-4"
+              style={{ top: "100%", left: 0 }}
+            >
+              Notification
+            </div>
+          )}
+        </div>
+
+        <div className="absolute ml-64 ">
+          <img
+            className="log w-13 ml-0 mb-8 "
+            src={props.elsrc2}
+            alt={props.elalt2}
           />
         </div>
       </div>
